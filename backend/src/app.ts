ab78411cdd8cec14b4e2b575fa009ede1d918d68
Alexis Checura <alexischecura@gmail.com>
2023-11-14 16:45:13 -0300

    feat/add cookie parser and cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { envVars } from './configs/envConfig';
 import userRoutes from './routes/userRoutes';
 import globalErrorHandler from './controllers/errorController';
 
 const app = express();
 
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: envVars.ORIGIN,
+    credentials: true,
+  })
+);
 
 app.use('/api/v1/users', userRoutes);
 
