09de9b90ef49bd77593bff70e9524471b02b4630
Alexis Checura <alexischecura@gmail.com>
2023-11-23 11:25:20 -0300

    feat/add get current user function

--- frontend/src/api/instagramApi.ts
+++ frontend/src/api/instagramApi.ts
@@ -1,19 +1,26 @@
 import axios from 'axios';
 import { getEnvVariables } from '../helpers/getEnvVariables';
-import { LoginUserType, LoginUserResponse } from '../types/user';
+import { LoginUserType, LoginUserResponse, User } from '../types/user';
 
 const { VITE_API_URL } = getEnvVariables();
 
 const instagramApi = axios.create({
   baseURL: VITE_API_URL,
+  withCredentials: true,
 });
 
 export const loginUser = async ({ identifier, password }: LoginUserType) => {
-  const { data } = await instagramApi.post('/users/login', {
+  const { data } = await instagramApi.post<LoginUserResponse>('/users/login', {
     identifier,
     password,
   });
-  return data as LoginUserResponse;
+  return data;
+};
+
+export const getCurrentUser = async () => {
+  const user = await instagramApi.get<User>('/users/me');
+
+  return user;
 };
 
 export default instagramApi;
