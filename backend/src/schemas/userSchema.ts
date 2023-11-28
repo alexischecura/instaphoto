0e90e15db902255b038d8edb5caaa7e056d6f36b
Alexis Checura <alexischecura@gmail.com>
2023-11-28 19:49:23 -0300

    refactor/remove password confirm requeriment

--- backend/src/schemas/userSchema.ts
+++ backend/src/schemas/userSchema.ts
@@ -1,44 +1,36 @@
 import { z } from 'zod';
 
-export const createUserSchema = z
-  .object({
-    fullName: z
-      .string({
-        required_error: 'Full name is required',
-        invalid_type_error: 'Full name must be a string',
-      })
-      .max(255)
-      .regex(/^[a-zA-Z ]*$/),
-    username: z
-      .string({
-        required_error: 'Username is required',
-        invalid_type_error: 'Username must be a string',
-      })
-      .max(255)
-      .regex(/^[a-zA-Z ]*$/),
-    email: z
-      .string({
-        required_error: 'E-mail is required',
-        invalid_type_error: 'E-mail must be a string',
-      })
-      .max(255)
-      .email('Invalid email address'),
+export const createUserSchema = z.object({
+  fullName: z
+    .string({
+      required_error: 'Full name is required',
+      invalid_type_error: 'Full name must be a string',
+    })
+    .max(255)
+    .regex(/^[a-zA-Z ]*$/),
+  username: z
+    .string({
+      required_error: 'Username is required',
+      invalid_type_error: 'Username must be a string',
+    })
+    .max(255)
+    .regex(/^[a-zA-Z ]*$/),
+  email: z
+    .string({
+      required_error: 'E-mail is required',
+      invalid_type_error: 'E-mail must be a string',
+    })
+    .max(255)
+    .email('Invalid email address'),
 
-    password: z
-      .string({
-        required_error: 'Please provide your password',
-        invalid_type_error: 'Password must be a string',
-      })
-      .min(8, 'Password must be more than 8 characters')
-      .max(32, 'Password too long, please provide a shorter password'),
-    passwordConfirm: z.string({
-      required_error: 'Please confirm your password',
-    }),
-  })
-  .refine((user) => user.password === user.passwordConfirm, {
-    message: "Passwords don't match",
-    path: ['passwordConfirm'],
-  });
+  password: z
+    .string({
+      required_error: 'Please provide your password',
+      invalid_type_error: 'Password must be a string',
+    })
+    .min(8, 'Password must be more than 8 characters')
+    .max(32, 'Password too long, please provide a shorter password'),
+});
 
 export type CreateUserType = Omit<
   z.infer<typeof createUserSchema>,
