4d71689a96038405713782e7ff9829d5d343fdcf
Alexis Checura <alexischecura@gmail.com>
2023-12-04 11:49:19 -0300

    feat/add routes for each section

--- frontend/src/router/InstagramRouter.tsx
+++ frontend/src/router/InstagramRouter.tsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import LoadingPage from '../components/common/LoadingPage';
 import ProtectedRoute from '../components/auth/ProtectedRoute';
 import AppLayout from '../components/layout/AppLayout';
+import HomePage from '../pages/HomePage';
 
 const LoginPage = lazy(() => import('../pages/LoginPage'));
 const SignUpPage = lazy(() => import('../pages/SignUpPage'));
@@ -21,7 +22,13 @@ function AppRouter() {
             </ProtectedRoute>
           }
         >
-          <Route path="/" element={<h1>Is login</h1>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<h1>Search</h1>} />
+          <Route path="/explore" element={<h1>Explore</h1>} />
+          <Route path="/message" element={<h1>Messages</h1>} />
+          <Route path="/notifications" element={<h1>Notifications</h1>} />
+          <Route path="/create" element={<h1>Create</h1>} />
+          <Route path="/:username" element={<h1>USER</h1>} />
           <Route path="/*" element={<Navigate to="/" />} />
         </Route>
         <Route path="/login" element={<LoginPage />} />
