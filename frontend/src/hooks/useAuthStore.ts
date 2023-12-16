39561d44a3bd90fade978f511a4f26eb89e99b02
Alexis Checura <alexischecura@gmail.com>
2023-12-16 10:44:13 -0300

    refactor/remove log

--- frontend/src/hooks/useAuthStore.ts
+++ frontend/src/hooks/useAuthStore.ts
@@ -93,7 +93,6 @@ export const useAuthStore = () => {
         dispatch(userLoggedOut('An unknown error occurred.'));
       }
     }
-    console.log(verificationCode);
   };
 
   const checkAuthToken = async () => {
