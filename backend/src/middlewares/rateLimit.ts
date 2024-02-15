0e1800c1d472e55b995c865ba519824935ddd13f
Alexis Checura <alexischecura@gmail.com>
2024-02-15 12:01:13 -0300

    refactor/change rate limits

--- backend/src/middlewares/rateLimit.ts
+++ backend/src/middlewares/rateLimit.ts
@@ -1,8 +1,8 @@
 import expressRateLimit from 'express-rate-limit';
 
 export const rateLimit = expressRateLimit({
-  windowMs: 3600 * 1000,
-  max: 50,
+  windowMs: 1600 * 1000,
+  max: 200,
   message: 'Too many request from this IP, please try again in an hour',
   standardHeaders: 'draft-7',
   legacyHeaders: false,
